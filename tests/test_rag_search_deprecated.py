b5f10cddf90642d7f1c5f1242aa47c93d099c044
Franklin Silveira Baldo <franklinbaldo@gmail.com>
2025-10-08 21:10:45 -0400

    Remove legacy RAG modules and refresh integration tests

--- tests/test_rag_search_deprecated.py
+++
@@ -1,9 +0,0 @@
-"""Regression guard for the deprecated TF-IDF search module."""
-
-from importlib import import_module
-
-
-def test_rag_search_module_is_marked_deprecated() -> None:
-    module = import_module("egregora.rag.search")
-    assert module.__doc__
-    assert module.__doc__.startswith("DEPRECATED"), module.__doc__
