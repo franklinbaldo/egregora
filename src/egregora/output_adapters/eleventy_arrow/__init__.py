242dab4bac5ac9395ea951d80c543dfc413e4565
Franklin Baldo <franklinbaldo@gmail.com>
2025-11-16 08:10:43 -0400

    refactor(output): package adapters

---
+++ src/egregora/output_adapters/eleventy_arrow/__init__.py
@@ -0,0 +1,8 @@
+"""Eleventy Arrow output adapter package."""
+
+from egregora.output_adapters.eleventy_arrow.adapter import (
+    EleventyArrowAdapter,
+    EleventyArrowOutputAdapter,
+)
+
+__all__ = ["EleventyArrowAdapter", "EleventyArrowOutputAdapter"]
